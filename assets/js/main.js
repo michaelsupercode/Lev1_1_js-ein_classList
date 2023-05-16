5a696cea5696dd2a4f32d8ee942c50997336f637
Michael Hanel <michaelsupercode@gmail.com>
2023-05-16 17:32:42 +0200

    lite rev

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,11 +4,6 @@ h1.classList.add("headline")
 ul.classList.add("liste");
 
 function changeHeadline() {
-    h1.classList.toggle("idhead");
     ul.classList.toggle("liste");
 }
 
-function resetHeadline() {
-    h1.classList.toggle("idhead");
-    ul.classList.toggle("liste");
-}
